f3f79885be36dc04093d6edf83a4d56e2d9c4678
sanobar17 <nuritdinovasanobar8@gmail.com>
2025-04-11 15:57:13 +0500

    Update first.sql

--- l1/homework/first.sql
+++ l1/homework/first.sql
@@ -1,1 +1,5 @@
-data is collection of facts and numbers.
+##Easy
+1. data is a collection of facts and information. database is also collection of data which is organized. A relational database organizes data into tables with rows and columns. A table is organizing data into rows and columns.
+2. In-Memory OLTP,  strong data manipulation capabilities, robust query processing, data security measures and always encrypted.
+3. Windows Authentication and Mixed Mode
+
